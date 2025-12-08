7b6720931883b17a6b9aa84fe09707c5639c081b
Rahinmon2903 <rahin2903@gmail.com>
2025-12-08 11:17:33 +0530

    middleware added and reset password completed

--- Controller/userController.js
+++ Controller/userController.js
@@ -1,4 +1,6 @@
 import Users from "../Model/userSchema.js";
+import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken";
 
 
 //register
@@ -6,20 +8,71 @@ import Users from "../Model/userSchema.js";
 export const userRegister=async (req,res) => {
     try {
         //1st method
-        // const {username,email,password}=req.body;
-        // const newUser=new Users({username,email,password});
-        // await newUser.save();
-        //  res.status(200).send({message:"user  registered successfully"})
+        const {username,email,password}=req.body;
+        const hashpassword=await bcrypt.hash(password,10) 
+        const newUser=new Users({username,email,password:hashpassword});
+        await newUser.save();
+         res.status(200).send({message:"user  registered successfully",data:newUser})
 
         //2nd method
-         const newUser=new Users(req.body);
-         await newUser.save();
-         res.status(200).send({message:"user  registered successfully",data:newUser})
+        //  const newUser=new Users(req.body);
+        //  await newUser.save();
+        //  res.status(200).send({message:"user  registered successfully",data:newUser})
 
 
         
     } catch (error) {
         res.status(503).send({message:"user not registered error in user register"})
+        
+    }
+    
+}
+
+
+//login
+
+export const userLogin=async (req,res) => {
+    try {
+        
+        const {email,password}=req.body;
+        const matchemail=await Users.findOne({email});
+        if(!matchemail){
+            return  res.status(404).send({message:"invalid email"})
+
+        }
+        const matchpassword=await bcrypt.compare(password,matchemail.password)
+       if(!matchpassword){
+        return  res.status(404).send({message:"invalid password"})
+
+       }
+
+       const token= jwt.sign({_id:matchemail._id},process.env.SECERT_KEY,{expiresIn:"1h"})
+       matchemail.token=token;
+       await matchemail.save();
+         res.status(200).send({message:"user  logged in successfully",token:token})
+
+
+        
+    } catch (error) {
+        res.status(503).send({message:"user not logged in error in user login"})
+        
+    }
+    
+}
+
+
+//get all users
+
+export const getUsers=async (req,res) => {
+    try {
+        
+       const users=await Users.find();
+         res.status(200).send({message:"user data retireved successfully",data:users})
+
+
+        
+    } catch (error) {
+        res.status(503).send({message:"user nfound error in get user"})
         
     }
     
--- Router/userRouter.js
+++ Router/userRouter.js
@@ -1,9 +1,12 @@
 import express from "express";
-import { userRegister } from "../Controller/userController.js";
+import { getUsers, userLogin, userRegister } from "../Controller/userController.js";
+import { adminMiddleware } from "../Middleware/adminMiddleware.js";
 
 const router=express.Router();
 
 router.post("/register",userRegister)
+router.post("/login",userLogin)
+router.get("/getdata",adminMiddleware,getUsers)
 
 
 
